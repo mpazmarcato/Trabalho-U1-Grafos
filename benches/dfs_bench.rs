34e9e0f30d739f5b256e9ab322130970a85c8cbc
Vinicius Sampaio <vldsampaio@pm.me>
2025-09-20 17:31:48 -0300

    bench: adds dfs benchmark for adjacency_matrix and adjacency_list

---
+++ benches/dfs_bench.rs
@@ -0,0 +1,42 @@
+use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
+use graphs_algorithms::Graph;
+use graphs_algorithms::graphs::{AdjacencyList, AdjacencyMatrix};
+
+fn create_adjacency_matrix(size: usize) -> AdjacencyMatrix {
+    let mut matrix = vec![vec![0; size]; size];
+    for i in 0..size {
+        matrix[i][(i + 1) % size] = 1;
+        matrix[(i + 1) % size][i] = 1;
+    }
+    AdjacencyMatrix(matrix)
+}
+
+fn bench_dfs_matrix(c: &mut Criterion) {
+    let sizes = vec![10, 50, 100, 500];
+
+    for size in sizes {
+        let matrix = create_adjacency_matrix(size);
+        c.bench_with_input(
+            BenchmarkId::new("dfs_adjacency_matrix", size),
+            &size,
+            |b, _| b.iter(|| matrix.dfs()),
+        );
+    }
+}
+
+fn bench_dfs_list(c: &mut Criterion) {
+    let sizes = vec![10, 50, 100, 500];
+
+    for size in sizes {
+        let matrix = create_adjacency_matrix(size);
+        let list = AdjacencyList::from_adjacency_matrix(&matrix);
+        c.bench_with_input(
+            BenchmarkId::new("dfs_adjacency_list", size),
+            &size,
+            |b, _| b.iter(|| list.dfs(0)),
+        );
+    }
+}
+
+criterion_group!(benches, bench_dfs_matrix, bench_dfs_list);
+criterion_main!(benches);
